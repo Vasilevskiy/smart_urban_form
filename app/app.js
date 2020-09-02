2d38b74d7d8fef08143500f9046b5e8412cef5ff
Andrew Vasilevsky <avasilevskiy@netsolace.com>
2020-09-02 13:36:22 +0300

    changes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { sendMail } = require("./service/smtp");
+const { sendMail } = require("../service/smtp");
 const env = require("dotenv").config();
 
 var app = express();
